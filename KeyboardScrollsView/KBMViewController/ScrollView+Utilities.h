a45a2311ce16fed19738b7f169c6b5f90932096f
Scott Richards <scott.srichards@gmail.com>
2015-01-31 10:34:01 -0800

    Changes

--- KeyboardScrollsView/KBMViewController/ScrollView+Utilities.h
+++ KeyboardScrollsView/KBMViewController/ScrollView+Utilities.h
@@ -6,11 +6,10 @@
 //  Copyright 2011 Sofi Software LLC. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 
 @interface UIViewController (ScrollView_Utilities)
 
-//- (void) registerForKeyboardNotifications;
-//- (void) unregisterForKeyboardNotifications;
 - (void) scrollFieldIntoView:(UIView *)view rootView:(UIView *) rootView keyboardSize:(CGSize) keyboardSize scrollView:(UIScrollView *) scrollView;
 - (CGSize) insetScrollViewWhenKeyboardWasShown:(NSNotification*) aNotification scrollView:(UIScrollView *) scrollView;
 - (void) restoreScrollViewWhenKeyboardWillBeHidden:(NSNotification*)aNotification scrollView:(UIScrollView *) scrollView;
--- KeyboardScrollsView/ViewController.h
+++ KeyboardScrollsView/ViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "KBMViewController.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : KBMViewController
 
 
 @end
